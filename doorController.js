bd01ec90a593763dc248d0d9ee9da6a0a1aa5010
unknown <dev.sina2000@gmail.com>
2024-07-03 12:41:42 +0330

    update

--- doorController.js
+++ doorController.js
@@ -1,12 +1,12 @@
 const { Gpio } = require('pigpio');
 
-const doorPin = new Gpio(7, { mode: Gpio.OUTPUT }); // Use GPIO 18 for the door control
+const doorPin = new Gpio(4, { mode: Gpio.OUTPUT }); // Use GPIO 18 for the door control
 
 function openDoor() {
-  doorPin.digitalWrite(1); // Send a pulse to open the door
+  doorPin.digitalWrite(0); // Send a pulse to open the door
   setTimeout(() => {
-    doorPin.digitalWrite(0); // Turn off the pulse after 1 second
-  }, 1000); // Adjust the duration of the pulse as needed
+    doorPin.digitalWrite(1); // Turn off the pulse after 1 second
+  }, 2000); // Adjust the duration of the pulse as needed
 }
 
 module.exports = {
